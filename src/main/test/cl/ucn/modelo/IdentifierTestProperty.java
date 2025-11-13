4c8ec50f748345f37db0451b512a470a033ccfc1
dazztyn <greatdazz3@gmail.com>
2025-11-12 23:47:22 -0300

    mejora entregable 2

--- src/main/test/cl/ucn/modelo/IdentifierTestProperty.java
+++ src/main/test/cl/ucn/modelo/IdentifierTestProperty.java
@@ -3,6 +3,7 @@ package cl.ucn.modelo;
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.AlphaChars;
 import net.jqwik.api.constraints.Chars;
+import net.jqwik.api.constraints.NumericChars;
 import net.jqwik.api.constraints.StringLength;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -14,13 +15,9 @@ public class IdentifierTestProperty
     private Identifier id = new Identifier();
 
     @Property
-    //se que está feo, pido perdón profe
     public void validString(
             @ForAll @AlphaChars @StringLength(min = 1, max = 1) String first,
-            @ForAll @Chars({'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                    '0','1','2','3','4','5','6','7','8','9'})
-            @StringLength(min = 0, max = 4) String tail
+            @ForAll @AlphaChars @NumericChars @StringLength(min = 0, max = 4) String tail
     )
     {
         String s = first + tail;
@@ -33,8 +30,7 @@ public class IdentifierTestProperty
     @Property
     public void invalidString(
             @ForAll @AlphaChars @StringLength(min = 1, max = 1) String first,
-            @ForAll @Chars({'a','b','c','1','2','3','4','5','6','7','8','9','0'})
-            @StringLength(min = 5, max = 20) String tail
+            @ForAll @AlphaChars @NumericChars @StringLength(min = 5, max = 20) String tail
     )
     {
         String s = first + tail;
